40bf6995f88f2d5247b462f8d2d05387252d5ee3
michkrom <michkrom@gmail.com>
2013-05-15 18:50:38 -0700

    rebase local

---
+++ README.md
@@ -0,0 +1,14 @@
+#Projects related to the stm32f3discovery board from STMicro
+
+##Contains:
+  *lib - set of libraries for overall support of the board
+    *CMSIS - ARM CORTEX M3 support definitions
+      *+ STM32F30x_StdPeriph_Driver - CPU support package for STM32F30x
+      *+ STM32_USB-FS-Device_Driver - CPU support package for USB endpoint
+      *+ board - stm32f3discovery board specific files and utilities
+      *+ imulib - library of imu related functions
+    *+ proj - set of projects based on this board
+      *+ imutest - imu devices tests (includes fusion algs, corrections algs and output)
+
+##Notes
+Works contain parts of ARM's CSIMS and STMicro support libraries and related works by others.
--- lib/board/syscalls.c
+++ lib/board/syscalls.c
@@ -1,6 +1,6 @@
 /***********************************************************************/
 /*                                                                     */
-/*  SYSCALLS.C:  System Calls Remapping                                */
+/*  SYSCALLS.C:  System Calls Remapping for gcc/newlib                 */
 /*  most of this is from newlib-lpc and a Keil-demo                    */
 /*                                                                     */
 /*  these are "reentrant functions" as needed by                       */
@@ -9,6 +9,12 @@
 /*  TODO: some more work has to be done on this                        */
 /***********************************************************************/
 
+<<<<<<< HEAD
+=======
+#include <stdlib.h>
+#include <reent.h>
+#include <sys/stat.h>
+>>>>>>> d7c7073da17cfc360f35d3ebd6555c81e3cfc5cb
 #include "serial.h"
   
 #include "stm32f30x_conf.h"
@@ -68,129 +74,135 @@ void _ttywrch(int ch)
 
 unsigned __errno;
 
-  // new code for _read_r provided by Alexey Shusharin - Thanks
-  _ssize_t _read_r(struct _reent *r, int file, void *ptr, size_t len)
-  {
-    char c;
-    int  i;
-    unsigned char *p;
 
-    p = (unsigned char*)ptr;
+// new code for _read_r provided by Alexey Shusharin - Thanks
+_ssize_t _read_r(struct _reent *r, int file, void *ptr, size_t len)
+{
+  char c;
+  int  i;
+  unsigned char *p;
 
-    for (i = 0; i < len; i++)
-    {      
-      c = USART_ReceiveData(USART2);
-      *p++ = c;
-      USART_SendData(USART2, c);
+  p = (unsigned char*)ptr;
 
-      if (c == 0x0D && i <= (len - 2))
-      {
-	*p = 0x0A;
-	USART_SendData(USART2, 0x0A);
-	return i + 2;
-      }
+  for (i = 0; i < len; i++)
+  {      
+    c = USART_ReceiveData(USART2);
+    *p++ = c;
+    USART_SendData(USART2, c);
+
+    if (c == 0x0D && i <= (len - 2))
+    {
+      *p = 0x0A;
+      USART_SendData(USART2, 0x0A);
+      return i + 2;
     }
-    return i;
   }
-
-
-  _ssize_t _write_r (
-      struct _reent *r, 
-      int file, 
-      const void *ptr, 
-      size_t len)
-  {
-	  int i;
-	  const unsigned char *p;
-	  
-	  p = (const unsigned char*) ptr;
-	  
-	  for (i = 0; i < len; i++) {
-		  if (*p == '\n' ) USART_SendData(USART2,'\r');
-		  USART_SendData(USART2, *p++);
-	  }
-	  
-	  return len;
-  }
-
-  int _close_r(
-      struct _reent *r, 
-      int file)
-  {
-	  return 0;
-  }
-  
-  _off_t _lseek_r(
-      struct _reent *r, 
-      int file, 
-      _off_t ptr, 
-      int dir)
-  {
-	  return (_off_t)0;	/*  Always indicate we are at file beginning.	*/
-  }
-
-
-  int _fstat_r(
-      struct _reent *r, 
-      int file, 
-      struct stat *st)
-  {
-	  /*  Always set as character device.				*/
-	  st->st_mode = S_IFCHR;	
-		  /* assigned to strong type with implicit 	*/
-		  /* signed/unsigned conversion.  Required by 	*/
-		  /* newlib.					*/
-
-	  return 0;
-  }
-
-  int isatty(int file); /* avoid warning */
-
-  int _isatty(int file)
-  {
-	  return 1;
-  }
-
-  #if 0
-  static void _exit (int n) {
-  label:  goto label; /* endless loop */
-  }
-  #endif 
-
-  /* "malloc clue function" */
-
-  /**** Locally used variables. ****/
-  extern char end[];              /*  end is set in the linker command 	*/
-				  /* file and is the end of statically 	*/
-				  /* allocated data (thus start of heap).	*/
-
-  static char *heap_ptr;		/* Points to current end of the heap.	*/
-
-  /************************** _sbrk_r *************************************/
-  /*  Support function.  Adjusts end of heap to provide more memory to	*/
-  /* memory allocator. Simple and dumb with no sanity checks.		*/
-  /*  struct _reent *r	-- re-entrancy structure, used by newlib to 	*/
-  /*			support multiple threads of operation.		*/
-  /*  ptrdiff_t nbytes	-- number of bytes to add.			*/
-  /*  Returns pointer to start of new heap area.				*/
-  /*  Note:  This implementation is not thread safe (despite taking a	*/
-  /* _reent structure as a parameter).  					*/
-  /*  Since _s_r is not used in the current implementation, the following	*/
-  /* messages must be suppressed.						*/
-
-  void * _sbrk_r(
-      struct _reent *_s_r, 
-      ptrdiff_t nbytes)
-  {
-	  char  *base;		/*  errno should be set to  ENOMEM on error	*/
-
-	  if (!heap_ptr) {	/*  Initialize if first time through.		*/
-		  heap_ptr = end;
-	  }
-	  base = heap_ptr;	/*  Point to end of heap.			*/
-	  heap_ptr += nbytes;	/*  Increase heap.				*/
-	  
-	  return base;		/*  Return pointer to start of new heap area.	*/
-  }
+<<<<<<< HEAD
 
 #endif
+=======
+  return i;
+}
+
+
+_ssize_t _write_r (
+    struct _reent *r, 
+    int file, 
+    const void *ptr, 
+    size_t len)
+{
+	int i;
+	const unsigned char *p;
+	
+	p = (const unsigned char*) ptr;
+	
+	for (i = 0; i < len; i++) {
+		if (*p == '\n' ) USART_SendData(USART2,'\r');
+		USART_SendData(USART2, *p++);
+	}
+	
+	return len;
+}
+
+
+int _close_r(
+    struct _reent *r, 
+    int file)
+{
+	return 0;
+}
+
+
+_off_t _lseek_r(
+    struct _reent *r, 
+    int file, 
+    _off_t ptr, 
+    int dir)
+{
+	return (_off_t)0;	/*  Always indicate we are at file beginning.	*/
+}
+
+
+int _fstat_r(
+    struct _reent *r, 
+    int file, 
+    struct stat *st)
+{
+	/*  Always set as character device.				*/
+	st->st_mode = S_IFCHR;	
+		/* assigned to strong type with implicit 	*/
+		/* signed/unsigned conversion.  Required by 	*/
+		/* newlib.					*/
+
+	return 0;
+}
+
+int isatty(int file); /* avoid warning */
+
+int _isatty(int file)
+{
+	return 1;
+}
+
+#if 0
+static void _exit (int n) {
+label:  goto label; /* endless loop */
+}
+#endif 
+
+/* "malloc clue function" */
+
+/**** Locally used variables. ****/
+extern char end[];              /*  end is set in the linker command 	*/
+				/* file and is the end of statically 	*/
+				/* allocated data (thus start of heap).	*/
+
+static char *heap_ptr;		/* Points to current end of the heap.	*/
+
+/************************** _sbrk_r *************************************/
+/*  Support function.  Adjusts end of heap to provide more memory to	*/
+/* memory allocator. Simple and dumb with no sanity checks.		*/
+/*  struct _reent *r	-- re-entrancy structure, used by newlib to 	*/
+/*			support multiple threads of operation.		*/
+/*  ptrdiff_t nbytes	-- number of bytes to add.			*/
+/*  Returns pointer to start of new heap area.				*/
+/*  Note:  This implementation is not thread safe (despite taking a	*/
+/* _reent structure as a parameter).  					*/
+/*  Since _s_r is not used in the current implementation, the following	*/
+/* messages must be suppressed.						*/
+
+void * _sbrk_r(
+    struct _reent *_s_r, 
+    ptrdiff_t nbytes)
+{
+	char  *base;		/*  errno should be set to  ENOMEM on error	*/
+
+	if (!heap_ptr) {	/*  Initialize if first time through.		*/
+		heap_ptr = end;
+	}
+	base = heap_ptr;	/*  Point to end of heap.			*/
+	heap_ptr += nbytes;	/*  Increase heap.				*/
+	
+	return base;		/*  Return pointer to start of new heap area.	*/
+}
+>>>>>>> d7c7073da17cfc360f35d3ebd6555c81e3cfc5cb
